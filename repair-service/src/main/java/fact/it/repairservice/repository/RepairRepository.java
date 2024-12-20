755dea635bdb44c9862aee81decc8fa2e26acc80
Moos-Vanlangendonck <67058923+Moos-Vanlangendonck@users.noreply.github.com>
2024-12-20 16:29:36 +0100

    Delete repair-service directory

--- repair-service/src/main/java/fact/it/repairservice/repository/RepairRepository.java
+++
@@ -1,7 +0,0 @@
-package fact.it.repairservice.repository;
-
-import fact.it.repairservice.model.Repair;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepairRepository extends JpaRepository<Repair, Long> {
-}
