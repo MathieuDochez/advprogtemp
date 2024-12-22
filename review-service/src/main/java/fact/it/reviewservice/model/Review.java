1092467c8081c69af47d202c544af52a8faefdb9
Moos Vanlangendonck <r0879857@student.thomasmore.be>
2024-12-22 15:43:16 +0100

    small fix

--- review-service/src/main/java/fact/it/reviewservice/model/Review.java
+++ review-service/src/main/java/fact/it/reviewservice/model/Review.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(value = "review")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Review {
+    @Id
     private String id;
     private String comment;
     private Integer rating;
