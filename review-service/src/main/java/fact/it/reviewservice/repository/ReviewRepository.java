63dba26d0969e9ae38387c3d0afddd39408bfa01
MathieuDochez <r0893166@student.thomasmore.be>
2024-12-22 16:20:41 +0100

    removed transaction form repository

--- review-service/src/main/java/fact/it/reviewservice/repository/ReviewRepository.java
+++ review-service/src/main/java/fact/it/reviewservice/repository/ReviewRepository.java
@@ -1,13 +1,9 @@
 package fact.it.reviewservice.repository;
 
 import fact.it.reviewservice.model.Review;
-import jakarta.transaction.Transactional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-@Transactional
 public interface ReviewRepository extends MongoRepository<Review, String> {
 }
